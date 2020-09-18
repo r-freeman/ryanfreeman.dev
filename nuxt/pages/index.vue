d3459804f36e27b83057cac8049f5dd1cff32a4a
Ryan Freeman <N00173767@student.iadt.ie>
2020-09-18 23:12:58 +0100

    More styling

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="lg:w-2/3">
-        <main class="p-10">
+        <main class="px-10 lg:p-10">
             <Post v-for="post in posts" :key="post.id" :post="post" />
         </main>
     </div>
