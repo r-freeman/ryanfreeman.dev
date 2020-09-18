933db82b292be32fcdc76e78c269da898b12cf91
Ryan Freeman <N00173767@student.iadt.ie>
2020-09-18 18:02:38 +0100

    Added meta tags to nuxt.config.js

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -14,14 +14,19 @@ export default {
     ** See https://nuxtjs.org/api/configuration-head
     */
     head: {
-        title: process.env.npm_package_name || '',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        title: 'Ryan Freeman - Student Software Developer IADT',
         meta: [
-            {charset: 'utf-8'},
-            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-            {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { name: 'robots', content: 'index, follow' },
+            { name: "author", content: 'Ryan Freeman' },
+            // { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
         ],
         link: [
-            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ]
     },
     /*
