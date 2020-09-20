7d0e27e7929678acba38f416999428c401444165
Ryan Freeman <N00173767@student.iadt.ie>
2020-09-21 00:39:31 +0100

    Added Button component

---
+++ nuxt/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <nuxt-link
+        :to="buttonLink"
+        class="text-base border border-gray-300 px-8 py-2 rounded-full leading-9 hover:text-blueribbon"
+    >{{ buttonText }}</nuxt-link>
+</template>
+
+<script>
+export default {
+    props: {
+        buttonText: {
+            type: String,
+            required: true,
+        },
+        buttonLink: {
+            type: String,
+            required: false,
+            default: "/",
+        },
+    },
+};
+</script>
