f3cc248b4ab5c619e20000ce196207f06970aed5
Ryan Freeman <N00173767@student.iadt.ie>
2020-09-21 22:43:23 +0100

    Added Pages template

---
+++ nuxt/pages/pages/_slug.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="lg:w-2/3">
+        <main class="px-8 lg:p-10">
+            <h1
+                class="mb-8 font-semibold font-sans text-4xl leading-tight antialiased"
+            >{{ page.title.rendered }}</h1>
+            <section v-html="page.content.rendered"></section>
+        </main>
+    </div>
+</template>
+
+<script>
+import { stripHtml } from "@/utils";
+
+export default {
+    head() {
+        return {
+            title: `${this.page.title.rendered} - Ryan Freeman - Student Software Developer IADT`,
+            meta: [
+                {
+                    name: "description",
+                    content: stripHtml(this.page.excerpt.rendered) || "",
+                    hid: "description",
+                },
+            ],
+        };
+    },
+    data() {
+        return {
+            slug: this.$route.params.slug,
+        };
+    },
+    computed: {
+        pages() {
+            return this.$store.state.wp.pages;
+        },
+        page() {
+            return this.pages.find((page) => page.slug === this.slug);
+        },
+    },
+};
+</script>
+
+<style scoped>
+>>> section p {
+    @apply mt-4 mb-6 text-base leading-relaxed;
+}
+
+>>> section p a {
+    @apply text-blueribbon;
+
+    &:hover {
+        @apply border-b border-blueribbon;
+    }
+}
+
+>>> section h2 {
+    @apply mt-12 mb-4 font-semibold text-2xl antialiased;
+}
+</style>
